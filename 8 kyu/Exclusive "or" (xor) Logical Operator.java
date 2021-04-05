7d6e60bb90dd80396052444bdc66be46f4377dae
Juan Daniel Cavero Tovar <69739890+danielex1999@users.noreply.github.com>
2021-04-05 14:13:30 -0500

    Create Exclusive "or" (xor) Logical Operator.java

---
+++ 8 kyu/Exclusive "or" (xor) Logical Operator.java
@@ -0,0 +1,23 @@
+public class XOR {
+    
+    public static boolean xor(boolean a, boolean b) {
+        if(a==true && b==false){
+          return true;
+        }else if(a==false && b==true){
+          return true;
+        }else{
+          return false;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
