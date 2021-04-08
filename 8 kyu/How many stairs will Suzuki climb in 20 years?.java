490a7a351bfe35cf75ab19088fdaaf7aceb74c95
Juan Daniel Cavero Tovar <69739890+danielex1999@users.noreply.github.com>
2021-04-08 15:02:12 -0500

    Create How many stairs will Suzuki climb in 20 years?.java

---
+++ 8 kyu/How many stairs will Suzuki climb in 20 years?.java
@@ -0,0 +1,16 @@
+public class Kata
+{
+    public static long stairsIn20(int[][] stairs){
+       long result = 0;
+       for (int i = 0; i < stairs.length; i++) {
+         for (int x = 0; x < stairs[i].length; x++) {
+           result += stairs[i][x];
+         }
+       }
+       return result*20;
+    }
+}
+
+
+
+
