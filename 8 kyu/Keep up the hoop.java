43d35b96fe76eb4e0c8a7a7b88e0b28091865b8f
Juan Daniel Cavero Tovar <69739890+danielex1999@users.noreply.github.com>
2021-04-14 12:19:36 -0500

    Create Keep up the hoop.java

---
+++ 8 kyu/Keep up the hoop.java
@@ -0,0 +1,19 @@
+public class HelpAlex{
+  public static String hoopCount(int n){
+   if(n<10){
+     return "Keep at it until you get it";
+   }else{
+     return "Great, now move on to tricks";
+   }
+  }
+}
+
+
+
+
+
+
+
+
+
+
